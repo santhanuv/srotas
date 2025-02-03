3e58a7b5f171b2b853af5fa5f41805c656b09927
Santhanu V <vinodsanthanu@gmail.com>
2025-02-03 09:23:05 +0530

    Add config file path when logging
    Logger logs the config file that is being executed along with other
    logs.

--- internal/log/logger.go
+++ internal/log/logger.go
@@ -9,9 +9,10 @@ import (
 )
 
 type Logger struct {
-	info  *log.Logger
-	debug *log.Logger
-	error *log.Logger
+	info       *log.Logger
+	debug      *log.Logger
+	error      *log.Logger
+	configName string
 }
 
 func New(info, debug, error io.Writer) *Logger {
@@ -24,16 +25,23 @@ func New(info, debug, error io.Writer) *Logger {
 	return l
 }
 
+func (l *Logger) SetForConfigFile(fileName string) {
+	l.configName = fileName
+}
+
 func (l *Logger) SetDebugWriter(debug io.Writer) {
-	l.debug = log.New(debug, "[Debug]: ", log.Ltime)
+	prefix := fmt.Sprintf("[Debug]: %s: ", l.configName)
+	l.debug = log.New(debug, prefix, log.Ltime)
 }
 
 func (l *Logger) SetInfoWriter(info io.Writer) {
-	l.info = log.New(info, "[Info]: ", log.Ltime)
+	prefix := fmt.Sprintf("[Info]: %s: ", l.configName)
+	l.info = log.New(info, prefix, log.Ltime)
 }
 
 func (l *Logger) SetErrorWriter(error io.Writer) {
-	l.error = log.New(error, "[Error]: ", log.Ltime)
+	prefix := fmt.Sprintf("[Error]: %s: ", l.configName)
+	l.error = log.New(error, prefix, log.Ltime)
 }
 
 func (l *Logger) Info(format string, args ...any) {
